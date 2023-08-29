154776dfa41a8d3dc5c3daf59e4478e7a7394a0c
MojahidJamal <105310797+MojahidJamal@users.noreply.github.com>
2023-08-29 12:54:43 +0200

    random quote implementation

--- lib/features/random_qutoe/presenation/cubit/random_quote_cubit.dart
+++ lib/features/random_qutoe/presenation/cubit/random_quote_cubit.dart
@@ -1,8 +1,36 @@
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:quotes/core/error/failures.dart';
+import 'package:quotes/core/usecases/usecase.dart';
+import 'package:quotes/core/utils/app_strings.dart';
+import 'package:quotes/features/random_qutoe/domain/entities/quotes.dart';
+import 'package:quotes/features/random_qutoe/domain/usecases/get_random_quote.dart';
 
 part 'random_quote_state.dart';
 
 class RandomQuoteCubit extends Cubit<RandomQuoteState> {
-  RandomQuoteCubit() : super(RandomQuoteInitial());
+  final GetRandomQuote getRandomQuoteUseCase;
+  RandomQuoteCubit({required this.getRandomQuoteUseCase})
+      : super(RandomQuoteInitial());
+
+  Future<void> getRandomQuote() async {
+    Either<Failure, Quotes> response = await getRandomQuoteUseCase(NoParams());
+
+    emit(response.fold(
+        (failure) => RandomQuoteError(msg: _mapFailureToMsg(failure)),
+        (quote) => RandomQuoteLoaded(quote: quote)));
+  }
+
+  String _mapFailureToMsg(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return AppStrings.serverFailure;
+      case CacheFailure:
+        return AppStrings.cacheFailure;
+
+      default:
+        return AppStrings.unexpectedError;
+    }
+  }
 }
